6d73fc3a3778fe2e7b92519a29c6b81906921fd3
Jason Woolnough <jason.woolnough@entelect.co.za>
2020-01-30 11:34:31 +0200

    Fixing java version issue

--- src/main/java/za/co/bootcamp/demo/animal/AnimalRepo.java
+++ src/main/java/za/co/bootcamp/demo/animal/AnimalRepo.java
@@ -1,23 +1,22 @@
 package za.co.bootcamp.demo.animal;
 
 
-import org.apache.derby.client.BasicClientDataSource;
-import za.co.bootcamp.demo.zoo.Zoo;
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 
 public class AnimalRepo {
-    private final BasicClientDataSource dataSource;
+    private final SQLServerDataSource dataSource;
 
     public AnimalRepo() {
         // TODO: Inject this dependency rather
-        dataSource = new BasicClientDataSource();
+        dataSource = new SQLServerDataSource();
         dataSource.setDatabaseName("MyDatabase");
         dataSource.setUser("me");
         dataSource.setPassword("password");
     }
 
     public Animal findByName(String name) {
-        System.out.println(String.format("Connecting to db with:\nDB name=%s\nuser=%s\npassword=%s",
-                dataSource.getDatabaseName(), dataSource.getUser(), dataSource.getPassword()));
+        System.out.println(String.format("Connecting to db with:\nDB name=%s\nuser=%s",
+                dataSource.getDatabaseName(), dataSource.getUser()));
         System.out.println("Getting animal with name = " + name);
 
         // Does a bunch of stuff we don't really care about right now.
--- src/main/java/za/co/bootcamp/demo/zoo/ZooRepo.java
+++ src/main/java/za/co/bootcamp/demo/zoo/ZooRepo.java
@@ -1,22 +1,22 @@
 package za.co.bootcamp.demo.zoo;
 
 
-import org.apache.derby.client.BasicClientDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 
 public class ZooRepo {
-    private final BasicClientDataSource dataSource;
+    private final SQLServerDataSource dataSource;
 
     public ZooRepo() {
         // TODO: Inject this dependency rather
-        dataSource = new BasicClientDataSource();
+        dataSource = new SQLServerDataSource();
         dataSource.setDatabaseName("MyDatabase");
         dataSource.setUser("me");
         dataSource.setPassword("password");
     }
 
     public Zoo findByName(String name) {
-        System.out.println(String.format("Connecting to db with:\nDB name=%s\nuser=%s\npassword=%s",
-                dataSource.getDatabaseName(), dataSource.getUser(), dataSource.getPassword()));
+        System.out.println(String.format("Connecting to db with:\nDB name=%s\nuser=%s",
+                dataSource.getDatabaseName(), dataSource.getUser()));
         System.out.println("Getting zoo with name = " + name);
 
         // Does a bunch of stuff we don't really care about right now.
