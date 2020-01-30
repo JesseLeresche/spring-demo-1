095d193a6eaf30eec2f56b7b873ced4869483645
Jason Woolnough <jason.woolnough@entelect.co.za>
2020-01-30 02:57:14 +0200

    Added empty config file

---
+++ src/main/java/za/co/bootcamp/demo/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package za.co.bootcamp.demo;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+    // TODO: your BasicClientDataSource bean goes here
+}
